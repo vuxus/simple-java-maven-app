2d8e935f936ff6ff0cf10a11b3487f788672746d
Ian Chong <vuxus.ix@gmail.com>
2023-01-16 16:43:59 +0800

    another commit for changing the comments

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Modified from local project !");
+        System.out.println("I LOVE Jenkins and it is actually easy to use !!!");
     }
 
     public String getMessage() {
