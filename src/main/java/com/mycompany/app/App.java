2362860cae15f6c139e1d4a0e7608bacdb119876
Ian Chong <vuxus.ix@gmail.com>
2023-01-16 16:27:19 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("today is a hot day !!!");
+        System.out.println("Modified from local project !");
     }
 
     public String getMessage() {
