1c7fb733135f792eccc0553b819dd3bb42fd8b01
Ian Chong <vuxus.ix@gmail.com>
2023-01-17 09:33:40 +0800

    made some changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I LOVE Jenkins and it is actually easy to use !!!");
+        System.out.println("I am trying to do something here !!!");
     }
 
     public String getMessage() {
